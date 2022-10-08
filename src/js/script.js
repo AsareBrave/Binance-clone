bbf1c08480f91e62031e883905c759a2d99a834b
unknown <asarebrave@gmail.com>
2022-10-08 22:41:01 +0100

    Initialize Binance App

---
+++ src/js/script.js
@@ -0,0 +1,121 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('side-menu')
+const close = document.getElementById('close-btn')
+let input = document.getElementById('user-input')
+const validate = document.getElementById('validate')
+const selectCountry = document.getElementById('select-country')
+const overlay = document.getElementById('overlay')
+
+
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('sidemenu')
+    menu.classList.toggle('w-0')
+    overlay.classList.replace('hidden', 'block')
+ 
+})
+close.addEventListener('click', () => {
+    menu.classList.toggle('sidemenu')
+    menu.classList.toggle('w-0')
+    overlay.classList.replace('block', 'hidden')
+})
+function borderGlowRed() {
+    input.classList.replace('border-myGreyShade', 'border-myRedValidation')
+    input.classList.replace('focus:border-myGold', 'focus:border-myRedValidation')
+    input.classList.replace('hover:border-myGold', 'hover:border-myRedValidation')
+}
+function borderGlowNormal() {
+    input.classList.replace('border-myRedValidation', 'border-myGreyShade')
+    input.classList.replace('focus:border-myRedValidation', 'focus:border-myGold')
+    input.classList.replace('hover:border-myRedValidation', 'hover:border-myGold')
+}
+function hideValidate() {
+    validate.classList.replace('block', 'hidden')
+}
+function showSelectCountry() {
+    selectCountry.classList.replace('hidden', 'block')
+}
+function hideSelectCountry() {
+    selectCountry.classList.replace('block', 'hidden')
+}
+function showValidate() {
+    validate.classList.replace('hidden', 'block')
+}
+
+function hideUserInput(usertext) {
+    let userInput = input.value
+    let firstChar = userInput.charAt(0);
+    let newSubstr
+    if (isNaN(userInput)) {
+        let getIndex = userInput.indexOf('@')
+        newSubstr = firstChar + "****" + userInput.substr(getIndex)
+        localStorage.setItem('user-input',newSubstr)
+    }
+    else {
+        let getInputLength = userInput.length;
+        let lgthMinus4 = getInputLength - 4;
+        let first3Num = userInput.substr(0, 3)
+        let last4Num = userInput.substr(lgthMinus4, getInputLength)
+        newSubstr = first3Num + "****" + last4Num
+        localStorage.setItem('user-input',newSubstr)
+    }
+}
+
+
+function textValidation() {
+    if (input.value == "") {
+        hideValidate()
+        borderGlowNormal()
+    }
+    if (input.value.length < 11 && !isNaN(input.value)) {
+        hideValidate()
+        borderGlowNormal()
+    }
+    // && isNaN(selectCountry)
+    if (input.value.length > 2 && !isNaN(input.value)) {
+        showSelectCountry()
+    }
+    if (input.value.length < 3 || isNaN(input.value)) {
+        hideSelectCountry()
+    }
+    if (input.value.length > 3 && isNaN(input.value)) {
+        hideSelectCountry()
+    }
+
+}
+
+function ValidateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input.value.length == 10 && !isNaN(input.value)) {
+        hideUserInput(input)
+        window.location="login-password.html"
+        return true
+
+    }
+    else if (input.value.match(validRegex) && input.value.length >= 10) {
+        hideUserInput(input)
+        window.location ="login-password.html"
+        return true
+
+    } else {
+        showValidate()
+        borderGlowRed()
+        return false
+    }
+
+}
+function validateBtn() {
+    if (input.value == "") {
+        showValidate()
+        borderGlowRed()
+    }
+
+    ValidateEmail(input);
+}
+function loadHiddenInput(){
+   document.getElementById('user-identity').innerHTML=localStorage.getItem('user-input')
+   document.getElementById('user-identity2').innerHTML=localStorage.getItem('user-input')
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      screen: {
+        sm: '480px',
+        md: '767px',
+        lg: '1020px',
+        xl: '1440px'
+      },
+      colors: {
+        myGreenLink: 'hsl(157, 87.1%, 42.5%)',
+        myWhite: 'hsl(255, 100%, 100%)',
+        myGold: 'hsl(44, 100%, 39.4%)',
+        myRedLight: 'hsl(47, 95%, 92.2%)',
+        myGreyLight: 'hsl(0, 0.0%, 96.1%)',
+        myGreyMid: 'hsl(217, 10.4%, 49.0%)',
+        myYellowishGold: 'rgb(240, 185, 11)',
+        myGreyShade: 'rgb(234, 236, 239)',
+        myGoldButton: 'rgb(252, 213, 53)',
+        myGoldLinkText: 'rgb(201, 148, 0)',
+        myDarkFooterText: 'rgb(71, 77, 87)',
+        myDarkText: 'rgb(30, 35, 41)',
+        myGreyImgbackground: 'rgb(245, 245, 245)',
+        myGreySvg: 'rgb(112, 112, 138)',
+        myRedValidation: 'rgb(207, 48, 74)',
+        myGreyOverlay: 'rgba(0,0,0,0.4)'
+      },
+
+    },
+    plugins: [],
+  }
+}
